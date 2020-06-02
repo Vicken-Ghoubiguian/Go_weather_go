2a350c820a375e5d7b075a36a9deac16068ce965
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2020-06-02 20:22:12 +0200

    Add a main.go file: begin

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+import (
+    "net/http"
+    "fmt"
+    "io/ioutil"
+    "os"
+)
+
+func main() {
+    resp, err := http.Get("https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=<api_key>")
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+
+    text, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+
+    fmt.Println(string(text))
+}
